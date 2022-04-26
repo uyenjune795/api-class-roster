dd3a64808a37a51fc5452ec06c12a3eae973ffab
uyenjune795 <80790327+uyenjune795@users.noreply.github.com>
2022-04-25 23:43:15 -0700

    Updated 4.25.22
    Fetched student names, add CSS grid - just the bare minimum, will work on adjusting the layout later

--- js/scripts.js
+++ js/scripts.js
@@ -21,7 +21,7 @@ fetch(url)
       // template
       const template = `
           <figure>
-            <figcaption>Student</figcaption>
+            <figcaption> ${student.name} </figcaption>
             <img src="${student.image.url}" alt="Placeholder" />
           </figure>
        `;
